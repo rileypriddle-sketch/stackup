2ec6e64f3c6f512274cfd4b18f478fca61a5fa41
Adrijan-Petek <adrijangithub@gmail.com>
2026-02-05 20:37:29 +0100

    fix: dynamic client page for export

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import ClientPage from "./ClientPage";
+import dynamic from "next/dynamic";
+
+const ClientPage = dynamic(() => import("./ClientPage"), {
+  ssr: false,
+});
 
 export default function Home() {
   return <ClientPage />;
